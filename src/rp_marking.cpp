1c42e2fb96d6ba0c8a7bbc383eac2afcb3027e2a
ljt12138 <ljt12138@qq.com>
2019-04-02 11:30:30 +0800

    docs(page_replacement): add proof for marking
    add proof of marking alogrithm.

--- src/rp_marking.cpp
+++ src/rp_marking.cpp
@@ -4,15 +4,34 @@
 class rp_marking : public page_rp {
 
 	set<int> unmarked;
+	set<int> marked;
 	
 public:
 
+	virtual void reset_hook(int n)
+	{
+		unmarked.clear();
+		marked.clear();
+	}
+	
 	rp_marking()
 	{
 		memset(name, 0, sizeof name);
-		strcpy(name, "marking");
+		strcpy(name, "Random Marking");
 	}
 
+	virtual void write_hook(size_t pos)
+	{
+	}
+
+	virtual void read_hook(size_t pos)
+	{
+		
+	}
+
+	virtual size_t find_swap()
+	{
+		
+	}
 	
-	
-};
+} rp_marking_algo;
