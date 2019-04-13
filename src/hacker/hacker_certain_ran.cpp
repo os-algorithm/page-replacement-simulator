27df44b1b8f8a97e4938664a50a8068ba8503147
ljt12138 <ljt12138@qq.com>
2019-04-13 17:15:26 +0800

    docs(page_replacement); add proof for lower bound

--- src/hacker/hacker_certain_ran.cpp
+++ src/hacker/hacker_certain_ran.cpp
@@ -10,11 +10,11 @@ public:
 	virtual void main(Simulater *sim)
 	{
 		this->sim = sim;
-		size_t len = pgsize * 129;
+		size_t len = pgsize * 150;
 		Array arr = sim->new_array(len);
 		size_t last = 0;
 		for (int i = 0; i < 1 << 20; i++) {
-			arr[(rand() % 129) * pgsize] = 1;
+			arr[1ll * i * pgsize % len] = 1;
 		}
 	}
 } hacker_certain_ran;
